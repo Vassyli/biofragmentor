6599cd7e06e936e7feeb40ad4abe4faec2dd86d3
Basilius Sauter <basilius.sauter@gmail.com>
2015-12-28 10:10:50 +0100

    Updates documentary

--- biofragmentor/compound.py
+++ biofragmentor/compound.py
@@ -1,25 +1,52 @@
 import re
 
 class Compound:
-	formula = ""
-	listofelements = {}
-	name = ""
-	
-	def __init__(self, **kwargs):
-		self.name = kwargs["name"]
-		self.formula = kwargs["formula"]
-		self.interpretateFormula()
-		
-	def interpretateFormula(self):
-		self.listofelements = {}
-		# from http://stackoverflow.com/questions/9782835/break-string-into-list-elements-based-on-keywords
-		a = re.findall(r'[A-Z][a-z]*|\d+', re.sub('[A-Z][a-z]*(?![\da-z])', r'\g<0>1', self.formula))
-		for i in range(0, len(a), 2):
-			self.listofelements[a[i]] = int(a[i+1])
-		
-	def getExactMass(self, elementdata):
-		emass = 0.0
-		for element in self.listofelements:
-			emass+= elementdata[element].getExactMass() * self.listofelements[element]
-		#print(self.name, self.formula, emass)
-		return emass
+    """ Represents the sum formula of a compound without any structural
+        informations
+
+    Attributes:
+        name (str): Given name of the compound
+        formula (str): String repressentation of the sum formula
+        listofelements (dict): A dictionary containing the information of
+            the attribute formula with elements as keys and number of atoms as
+            values.
+
+    Arguments:
+        name (str): Given name of the compound
+        formula (str): String repressentation of the sum formula
+    """
+    formula = ""
+    listofelements = {}
+    name = ""
+
+    def __init__(self, **kwargs):
+        self.name = kwargs["name"]
+        self.formula = kwargs["formula"]
+        self.__interpretateFormula()
+
+    def __interpretateFormula(self):
+        """ Analyzes the chemical sum formula and converts it into a machine-readable
+            dictionary which gets stored in listofelements
+        """
+        self.listofelements = {}
+        # from http://stackoverflow.com/questions/9782835/break-string-into-list-elements-based-on-keywords
+        a = re.findall(r'[A-Z][a-z]*|\d+', re.sub('[A-Z][a-z]*(?![\da-z])', r'\g<0>1', self.formula))
+        for i in range(0, len(a), 2):
+            self.listofelements[a[i]] = int(a[i+1])
+
+    def getExactMass(self, elementdata):
+        """ Returns the exact mass of the compound according to elemental composition.
+            A table containing the physical informations of the elements is needed.
+
+        Arguments:
+            elementdata (dict): Dictionary containing elemental symbol : Element object
+                association
+
+        Returns:
+            (float): The exact mass of the compound
+        """
+        emass = 0.0
+        for element in self.listofelements:
+            emass+= elementdata[element].getExactMass() * self.listofelements[element]
+        #print(self.name, self.formula, emass)
+        return emass
--- biofragmentor/element.py
+++ biofragmentor/element.py
@@ -1,28 +1,60 @@
 from .isotope import Isotope
 
 class Element:
-	symbol = ""
-	name = ""
-	mass = 0.0
-	isotopes = []
-	
-	def __init__(self, symbol, name, z, mass):
-		self.symbol = symbol
-		self.name = name
-		self.mass = float(mass)
-		self.z = int(z)
-		self.isotopes = []
-		
-	def __repr__(self):
-		return "<Element <%s>: %s; mass=%f; z=%i; isotopes=%i>" % (self.symbol, self.name, self.mass, self.z, len(self.isotopes))
-		
-	def addIsotope(self, attribs):
-		self.isotopes.append(Isotope(**attribs))
-		
-	def getExactMass(self, mostabundant = True):
-		list = []
-		for isotope in self.isotopes:
-			list.append((isotope.mass, isotope.abundance))
-		list = sorted(list, key=lambda x: x[1])
-		if mostabundant == True:
-			return list[-1][0]
+    """ Contains information about an element
+
+    Attributes:
+        symbol (str): Elemental symbol (H, He, Cl, Pt)
+        name (str): The english name of the element (Hydrogen, Helium, Chlorine, Platinum)
+        z (int): atomic number of the element ("number of protons")
+        mass (float): Average mass of the element
+        isotopes (list): A list of all isotopes for this element
+
+    Arguments:
+        symbol (str): Elemental symbol
+        name (str): The english name of the element
+        z (int, str): Atomic number
+        mass (float, str): Average elemental mass
+
+    """
+    symbol = ""
+    name = ""
+    mass = 0.0
+    isotopes = []
+
+    def __init__(self, symbol, name, z, mass):
+        self.symbol = symbol
+        self.name = name
+        self.mass = float(mass)
+        self.z = int(z)
+        self.isotopes = []
+
+    def __repr__(self):
+        return "<Element <%s>: %s; mass=%f; z=%i; isotopes=%i>" % (self.symbol, self.name, self.mass, self.z, len(self.isotopes))
+
+    def addIsotope(self, attribs):
+        """ Adds an isotope to a element instance
+
+        Arguments:
+            attribs (dict): Dictionary containing arguments for the class Isotope
+        """
+        self.isotopes.append(Isotope(**attribs))
+
+    def getExactMass(self, mostabundant = True):
+        """ Returns the exact mass of an element
+
+        Note:
+            The argument "mostabundant" is not implemented yet.
+
+        Arguments:
+            mostabundant (bool): ***NOT IMPLEMENTED YET***
+
+        Returns:
+            (float) The exact mass of an element
+        """
+        list = []
+        for isotope in self.isotopes:
+            list.append((isotope.mass, isotope.abundance))
+        list = sorted(list, key=lambda x: x[1])
+        if mostabundant == True:
+            return list[-1][0]
