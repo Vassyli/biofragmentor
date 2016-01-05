74363fad968b8ef3b7f98d242e67d2d8d50e9369
Basilius Sauter <basilius.sauter@gmail.com>
2016-01-05 17:25:08 +0100

    Bugfix: Monomers added to sequence class
    Fixed an issue that monomers were added to the sequence class instead of
    the sequence object. This caused DNA and RNA to report wrong masses and
    no losses.

--- biofragmentor/data.py
+++ biofragmentor/data.py
@@ -9,44 +9,44 @@ from .sequence import Sequence
 from .monomer import Monomer
 
 class Data:
-	path = None
-	elements = {}
-	sequences = {}
-	monomers = {}
-	
-	def __init__(self, path):
-		self.path = path
-		# Initialize Databases
-		self.initAtoms()
-		self.initSequences()
-		self.initMonomers()
-		
-	def _getRoot(self, file):
-		return ET.parse(os.path.join(*[self.path, file])).getroot()
-		
-	def initAtoms(self):
-		elements = self._getRoot("atoms.xml")
-		for element in elements:
-			e = Element(**element.attrib)
-			self.elements[element.attrib["symbol"]] = e
-			# Add Isotopes
-			for isotopes in element:
-				e.addIsotope(isotopes.attrib)
-				
-	def initSequences(self):
-		sequences = self._getRoot("sequences.xml")
-		for sequence in sequences:
-			seq = Sequence.create(**sequence.attrib)
-			self.sequences[seq.id] = seq
-			
-	def initMonomers(self):
-		monomers = self._getRoot("monomers.xml")
-		for monomer in monomers:
-			mon = Monomer(**monomer.attrib)
-			# Add losses
-			for loss in monomer:
-				mon.addLoss(**loss.attrib)
-			self.monomers[mon.id] = mon
-			for seqid in  mon.sequence:
-				if seqid in self.sequences:
-					self.sequences[seqid].addMonomer(mon)
+    path = None
+    elements = {}
+    sequences = {}
+    monomers = {}
+
+    def __init__(self, path):
+        self.path = path
+        # Initialize Databases
+        self.initAtoms()
+        self.initSequences()
+        self.initMonomers()
+
+    def _getRoot(self, file):
+        return ET.parse(os.path.join(*[self.path, file])).getroot()
+
+    def initAtoms(self):
+        elements = self._getRoot("atoms.xml")
+        for element in elements:
+            e = Element(**element.attrib)
+            self.elements[element.attrib["symbol"]] = e
+            # Add Isotopes
+            for isotopes in element:
+                e.addIsotope(isotopes.attrib)
+
+    def initSequences(self):
+        sequences = self._getRoot("sequences.xml")
+        for sequence in sequences:
+            seq = Sequence.create(**sequence.attrib)
+            self.sequences[seq.id] = seq
+
+    def initMonomers(self):
+        monomers = self._getRoot("monomers.xml")
+        for monomer in monomers:
+            mon = Monomer(**monomer.attrib)
+            # Add losses
+            for loss in monomer:
+                mon.addLoss(**loss.attrib)
+            self.monomers[mon.id] = mon
+            for seqid in  mon.sequence:
+                if seqid in self.sequences:
+                    self.sequences[seqid].addMonomer(mon)
